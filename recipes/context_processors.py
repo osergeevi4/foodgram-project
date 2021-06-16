291eb2870e4160493ee44a5b2ffdda20cbb34b66
osergeevi4 <okodit@yandex.ru>
2021-06-16 15:42:14 +0300

    ВосстанавливÐаемсяÐ

--- recipes/context_processors.py
+++ recipes/context_processors.py
@@ -2,7 +2,6 @@ from .models import ShopingList
 
 
 def get_shop_list(request):
-    """ Количество рецептов в списке покупок. """
     if request.user.is_authenticated:
         shop_list_count = ShopingList.objects.filter(
             user=request.user
